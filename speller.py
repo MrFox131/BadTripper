36efdca33c35217dc50280b9c31685a5c519c380
slonser <slonser@slonser-linux>
2021-08-06 14:27:08 +0300

    add speller tester,forwarding

---
+++ speller.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import user_agent
+import requests
+import urllib
+def speller(url):
+    API_BASE = 'https://speller.yandex.net/services/spellservice.json/checkText?text='
+    source_code = requests.get(url,headers={'User-Agent':user_agent.get_user_agent()}).text
+    soup = BeautifulSoup(source_code)
+    text = soup.text
+    r = requests.get(API_BASE+urllib.parse.quote(text)).json()
+    if len(text.split() == 0:
+        return 0
+    return len(r)/len(text.split()) * 100
